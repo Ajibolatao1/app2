6cace390dad99d8233141087b4c8a53eadcea951
Ajibolatao1 <123806876+Ajibolatao1@users.noreply.github.com>
2024-10-20 22:31:12 +0800

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe" },
+];
+app.get("/", (req, res) => {
+    res.send({ message: "Welcome", users });
+});
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+// Start Server
+app.listen(5000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
+];
+
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome", users });
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+// Start Server
+app.listen(5000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
